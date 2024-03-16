114f7d9b431a1c570c500ec62dcafe86654ef60b
mahdi00khalili <mahdikhalili238@gmail.com>
2024-03-16 13:39:37 +0330

    Write a script that prints "Hello" immediately, waits 5 seconds, and then prints "Goodbye".

---
+++ nodejs/Simple_Timeout.js
@@ -0,0 +1,12 @@
+/*
+Write a script that prints "Hello" immediately, 
+waits 5 seconds, and then prints "Goodbye".
+*/
+
+
+console.log("Hello");
+
+// Wait for 5 seconds
+setTimeout(() => {
+  console.log("Goodbye");
+}, 5000);
